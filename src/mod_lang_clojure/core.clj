c378d4fdef1e0936301a50450abd5e147faf80f3
git <liuxiaoxi@shopex.cn>
2013-06-24 13:21:41 +0800

    init project structure.

---
+++ src/mod_lang_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns mod-lang-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/mod_lang_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns mod-lang-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [mod-lang-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
