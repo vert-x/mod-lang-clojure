d64d6a734682a5e80dd19e432e7d6efa7b274d88
Toby Crawley <toby@tcrawley.org>
2013-08-29 10:50:54 -0400

    Clean up integration tests that are no longer needed.

--- api/src/test/resources/v1.clj
+++
@@ -1,6 +0,0 @@
-(ns isolation.v1
-  (:require [vertx.eventbus :as eb]
-            [isolation.shared :as shared]))
-
-(println "v1: value is " @shared/a-value)
-(eb/send "isolation" (swap! shared/a-value inc))
