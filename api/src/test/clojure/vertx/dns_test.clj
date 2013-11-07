6f4b876146f86c0b6a314a2aff278743d5cbcd93
Toby Crawley <toby@tcrawley.org>
2013-11-07 14:16:55 -0500

    Fix dns tests to use mock dns server from testtools.

--- api/src/test/clojure/vertx/dns_test.clj
+++ api/src/test/clojure/vertx/dns_test.clj
@@ -19,46 +19,69 @@
             [clojure.test :refer :all])
   (:import org.vertx.java.core.dns.DnsException
            java.net.InetAddress
-           (org.vertx.java.core.dns MxRecord)))
+           org.vertx.java.core.dns.MxRecord
+           org.vertx.testtools.TestDnsServer))
 
 (use-fixtures :each t/as-embedded)
 
-(def v4-regex #"^(\d+\.){3}\d+$")
-(def v6-regex #"^([^:]+:){7}[^:]+$")
-
-(def server "8.8.8.8")
+(defn start-server [server]
+  (.start server)
+  (t/on-complete
+   #(.stop server))
+  (-> server
+      .getTransports
+      first
+      .getAcceptor
+      .getLocalAddress))
 
 (deftest lookup-with-no-type
-  (d/lookup server "google.com"
-            (fn [err r]
-              (t/test-complete
-               (is (nil? err))
-               (is (:address r))
-               (is (:basis r))
-               (is (instance? InetAddress (:basis r)))))))
+  (let [ip "10.0.0.1"]
+    (d/lookup (start-server
+               (TestDnsServer/testLookup ip))
+              "example.com"
+              (fn [err r]
+                (t/test-complete
+                 (is (nil? err))
+                 (is (= ip (:address r)))
+                 (is (instance? InetAddress (:basis r))))))))
 
 (deftest lookup-with-:any
-  (d/lookup server "google.com" :any
-            (fn [_ r]
-              (t/test-complete
-               (is (:address r))))))
+  (let [ip "10.0.0.1"]
+    (d/lookup (start-server
+               (TestDnsServer/testLookup ip))
+              "example.com" :any
+              (fn [err r]
+                (t/test-complete
+                 (is (nil? err))
+                 (is (= ip (:address r)))
+                 (is (instance? InetAddress (:basis r))))))))
 
 (deftest lookup-with-:ipv4
-  (d/lookup server "google.com" :ipv4
-            (fn [_ r]
-              (t/test-complete
-               (is (:address r))
-               (is (re-find v4-regex (:address r)))))))
+  (let [ip "10.0.0.1"]
+    (d/lookup (start-server
+               (TestDnsServer/testLookup4 ip))
+              "example.com" :ipv4
+              (fn [err r]
+                (t/test-complete
+                 (is (nil? err))
+                 (is (= ip (:address r)))
+                 (is (instance? InetAddress (:basis r))))))))
 
 (deftest lookup-with-:ipv6
-  (d/lookup server "google.com" :ipv6
-            (fn [_ r]
-              (t/test-complete
-               (is (:address r))
-               (is (re-find v6-regex (:address r)))))))
+  (let [ip "0:0:0:0:0:0:0:1"]
+    (d/lookup (start-server
+               (TestDnsServer/testLookup6))
+              "example.com" :ipv6
+              (fn [err r]
+                (t/test-complete
+                 (is (nil? err))
+                 (is (= ip (:address r)))
+                 (is (instance? InetAddress (:basis r))))))))
 
 (deftest lookup-with-invalid-host-name
-  (d/lookup server "this-should-never-resolve-i-hope.biscuit"
+  (d/lookup (start-server
+             (TestDnsServer/testLookupNonExisting))
+            "not-exist"
             (fn [err r]
               (t/test-complete
                (is err)
@@ -73,71 +96,98 @@
    (mapv check-fn r)))
 
 (deftest resolve-:A
-  (d/resolve server "google.com" :A
-             (partial check-resolve-result
+  (let [ip "10.0.0.1"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveA ip))
+               "example.com" :A
+               (partial check-resolve-result
                       (fn [r]
-                        (is (re-find v4-regex (:address r)))
-                        (is (instance? InetAddress (:basis r)))))))
+                        (is (= ip (:address r)))
+                        (is (instance? InetAddress (:basis r))))))))
 
 (deftest resolve-:AAAA
-  (d/resolve server "google.com" :AAAA
-             (partial check-resolve-result
+    (let [ip "0:0:0:0:0:0:0:1"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveAAAA "::1"))
+               "example.com" :AAAA
+               (partial check-resolve-result
                       (fn [r]
-                        (is (re-find v6-regex (:address r)))
-                        (is (instance? InetAddress (:basis r)))))))
+                        (is (= ip (:address r)))
+                        (is (instance? InetAddress (:basis r))))))))
 
 (deftest resolve-:CNAME
-  (d/resolve server "downloads.immutant.org" :CNAME
-             (partial check-resolve-result
-                      #(is (string? %)))))
+  (let [cname "cname.example.com"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveCNAME cname))
+               "example.com" :CNAME
+               (partial check-resolve-result
+                        #(is (= cname %))))))
 
 (deftest resolve-:MX
-  (d/resolve server "google.com" :MX
-             (partial check-resolve-result
-                      (fn [r]
-                        (is (:priority r))
-                        (is (:name r))
-                        (is (instance? MxRecord (:basis r)))))))
+  (let [mx "mail.example.com"
+        prio 10]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveMX prio mx))
+               "example.com" :MX
+               (partial check-resolve-result
+                        (fn [r]
+                          (is (= prio (:priority r)))
+                          (is (= mx (:name r)))
+                          (is (instance? MxRecord (:basis r))))))))
 
 (deftest resolve-:NS
-  (d/resolve server "google.com" :NS
-             (partial check-resolve-result
-                        #(is (string? %)))))
+  (let [ns "ns.example.com"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveNS ns))
+               "example.com" :NS
+               (partial check-resolve-result
+                        #(is (= ns %))))))
 
 (deftest resolve-:PTR
-  (d/resolve server "downloads.immutant.org" :PTR
-             (fn [err r]
-               (t/test-complete
-                (is (nil? err))
-                (is (string? r))))))
+  (let [ptr "ptr.example.com"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolvePTR ptr))
+               "example.com" :PTR
+               (fn [err r]
+                 (t/test-complete
+                  (is (nil? err))
+                  (is (= ptr r)))))))
 
-;; TODO: test SRV
+(deftest resolve-:SRV
+  (let [prio 10
+        weight 1
+        port 80
+        target "example.com"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveSRV prio weight port target))
+               "example.com" :SRV
+               (partial check-resolve-result
+                        (fn [r]
+                          (is (= prio (:priority r)))
+                          (is (= weight (:weight r)))
+                          (is (= port (:port r)))
+                          (is (= target (:target r))))))))
 
 (deftest resolve-:TXT
-  (d/resolve server "google.com" :TXT
-             (partial check-resolve-result
-                      #(is (string? %)))))
+  (let [txt "yadayada"]
+    (d/resolve (start-server
+                (TestDnsServer/testResolveTXT txt))
+               "example.com" :TXT
+               (partial check-resolve-result
+                        #(is (= txt %))))))
 
 (deftest reverse-lookup
-  (let [c server]
-    (d/lookup c "google.com"
-              (fn [_ {:keys [address]}]
-                (d/reverse-lookup c address
-                                  (fn [err {:keys [host basis]}]
-                                    (t/test-complete
-                                     (is (nil? err))
-                                     (is host)
-                                     (is basis)
-                                     (is (instance? InetAddress basis)))))))))
-
-(deftest reverse-lookup-for-nonexistent-address
-  (d/reverse-lookup server "123"
-                    (fn [err r]
-                      (t/test-complete
-                       (is err)
-                       (is (instance? DnsException (:basis err)))
-                       (is (= :NXDOMAIN (:type err)))
-                       (is (nil? r))))))
+  (let [addr "10.0.0.1"
+        ptr "ptr.example.com"]
+    (d/reverse-lookup
+     (start-server (TestDnsServer/testReverseLookup ptr))
+     addr
+     (fn [err {:keys [address host basis]}]
+       (t/test-complete
+        (is (nil? err))
+        (is (= ptr host))
+        (is (= addr address))
+        (is (instance? InetAddress basis)))))))
 
 (deftest sock-address-from-string
   (let [sock-address-from-string #'d/sock-address-from-string]
@@ -152,3 +202,7 @@
         (is (= 53 (.getPort addr))))))
   (t/test-complete))
 
+
+
+  
+  
