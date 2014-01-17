7d60746be9141a8b0ce3175342d3dbc1ceb37a23
Toby Crawley <toby@tcrawley.org>
2014-01-17 11:23:54 -0500

    Use vertxbus.js from master when building against a SNAPSHOT.

--- api/src/build/download_js_artifacts.clj
+++ api/src/build/download_js_artifacts.clj
@@ -4,14 +4,18 @@
 ;; download vertxbus and sockjs, so they can be available to the
 ;; ClojureScript compiler for downstream projects via deps.cljs.
 
-(let [artifacts {"vertxbus.js" "https://raw.github.com/eclipse/vert.x/v${version.vertx}/src/dist/client/vertxbus.js"
-                 "sockjs.js" "http://cdn.sockjs.org/sockjs-${version.sockjs}.js"}
+(let [artifacts {"vertxbus.js" ["${version.vertx}"
+                                "https://raw.github.com/eclipse/vert.x/v${version.vertx}/src/dist/client/vertxbus.js"
+                                "https://raw.github.com/eclipse/vert.x/master/src/dist/client/vertxbus.js"]
+                 "sockjs.js" ["${version.sockjs}"
+                              "http://cdn.sockjs.org/sockjs-${version.sockjs}.js"]}
       dest-dir (doto (io/file "target/classes/js")
                  .mkdirs)]
-  (doseq [[name url] artifacts]
+  (doseq [[name info] artifacts]
     (let [file (io/file dest-dir name)]
       (if (.exists file)
         (println (.getPath file) "already exists, skipping.")
-        (do
+        (let [[version release-url snapshot-url] info
+              url (if (re-find #"SNAPSHOT" version) snapshot-url release-url)]
           (println "Downloading" url "to" (.getPath file))
           (io/copy (io/reader url) file))))))
