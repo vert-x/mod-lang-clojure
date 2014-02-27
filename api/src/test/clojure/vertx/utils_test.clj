e576fedd82905266305ef937f3935bad7916eab2
Dean Pehrsson-Chapman <dean@p14n.com>
2014-02-27 22:45:23 +0000

    All tests working and type hints in
    Signed-off-by: Dean Pehrsson-Chapman <dean@p14n.com>

--- api/src/test/clojure/vertx/utils_test.clj
+++ api/src/test/clojure/vertx/utils_test.clj
@@ -43,18 +43,34 @@
     (is (= b (set-properties b {:jam "grape"})))
     (is (= "grape" (.getJam b)))))
 
-(deftest persistentmap-should-encode ;IPersistentMap
+(deftest persistentmap-should-encode 
   (is (= (encode {:a "b"}) (JsonObject. "{\"a\":\"b\"}"))))
 
-(deftest map-should-encode ;Map
+(deftest persistentset-should-encode
+  (let [encoded (encode #{:a "b"})]
+    (is (= JsonArray (type encoded)))
+    (is (= 2 (.size encoded)))
+    (is (.contains encoded "a"))
+    (is (.contains encoded "b"))))
+
+(deftest persistentmap-of-maps-should-encode
+  (is (= (encode {:a {:b "c"} }) (JsonObject. "{\"a\":{\"b\":\"c\"}}"))))
+
+(deftest map-should-encode
   (is (= (encode (let [m (java.util.HashMap.)] (.put m "a" "b") m)) (JsonObject. "{\"a\":\"b\"}"))))
 
-(deftest seq-should-encode ;Seqable
+(deftest seq-should-encode
   (is (= (encode '(1000000000000 2000000000000)) (JsonArray. "[1000000000000,2000000000000]"))))
 
-(deftest jsonarray-should-decode ;JsonArray
+(deftest jsonarray-should-decode
     (is (= [1,2] (decode (JsonArray. "[1,2]")))))
 
-(deftest jsonobject-should-decode ;JsonObject
+(deftest jsonobject-should-decode
   (is (= {:a "b"} (decode (JsonObject. "{\"a\":\"b\"}")))))
+
+(deftest map-of-maps-should-decode
+  (is (= {:a {:b "c"} } (decode (JsonObject. "{\"a\":{\"b\":\"c\"}}")))))
+
+(deftest map-of-mapped-nums-should-decode
+  (is (= {:a {:b 1000000000000} } (decode (JsonObject. "{\"a\":{\"b\":1000000000000}}")))))
 
